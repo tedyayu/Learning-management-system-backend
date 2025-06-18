44bb429415913ab0a6c59ea38fedb72a324cd767
tedyayu <tedyayu294@gmail.com>
2025-06-19 01:47:19 +0300

    hardcoded port is removed

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import prisma from "./utils/prismaInstance";
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
-const PORT:Number =Number(process.env.PORT) || 5000;
+const PORT:Number =Number(process.env.PORT) ;
 
 const app:Express=express()
 app.use(
